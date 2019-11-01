4dae0e0b75c12b9b7ac5ed1b688c193113f39613
Heansell Menchú <hdtahay@gmail.com>
2019-11-01 11:13:37 -0600

    Se agregó y se configuró las urls para los ficheros media

--- webempresa/urls.py
+++ webempresa/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
 
 urlpatterns = [
 
@@ -24,3 +25,8 @@ urlpatterns = [
     #Paths del administrador
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    from django.conf.urls.static import static 
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
